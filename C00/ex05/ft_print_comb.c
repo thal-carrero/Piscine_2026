879273d5945714a5b64a550edea4d4a041c03947
Thalia <thalia.carrero@icloud.com>
2026-07-02 01:35:30 +0200

    added the pdf and other stuff

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -1,17 +1,18 @@
 
 #include <unistd.h>
 
+/* Funtion to write the number */
 void write_num(char arr[])
 {
 	write(1, arr, 3);
 
-	if (arr[0] == '7' && arr[1] == '8' && arr[2] == '9')
+	if (arr[0] == '7' && arr[1] == '8' && arr[2] == '9') /* CHecking if im on the last number possible */
 	{
-		write(1, ".\n", 2);
+		write(1, ".\n", 2); /* If I am, write a final point and a new line  */
 	}
 	else 
 	{
-		write(1, ", ", 2);
+		write(1, ", ", 2); /* If Im not write a coma and a space to concatenate the next number */
 	}
 
 }
